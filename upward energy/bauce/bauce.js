aeba33099283d9b8d9733e0cd9eec477d3e1c130
Jason Ward <sirphobos88@gmail.com>
2011-03-14 19:18:09 -0500

    edited ‘upward energy/bauce/bauce.js’

--- upward energy/bauce/bauce.js
+++ upward energy/bauce/bauce.js
@@ -33,7 +33,7 @@ bauceO[bauceCount] = bauceOpacity;
 bauceZ[bauceCount] = baucezIndex;
 bauceT[bauceCount] = bauceTop;
 
-bauceWidth=bauceWidth*0.6;
+bauceWidth=bauceWidth*1.0;
 bauceOpacity=bauceOpacity*0.7;
 baucezIndex--
 bauceTop = bauceTop - (bauceTopGap*3);
